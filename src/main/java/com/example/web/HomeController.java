7f86aadb63e7729a22c003b9fc38f95c8cc55058
Junjie Yang <yjj31415926@126.com>
2015-12-09 09:42:40 +0800

    热启动 ctrl-F9

---
+++ src/main/java/com/example/web/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by yjj on 2015/12/9.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "home,world";
+    }
+}
